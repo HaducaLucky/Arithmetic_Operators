ecd5969f5f8f88cb3b92d9aaad75d09b0e588146
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-17 11:21:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//  arithmetic operators = operands (values, variables, etc.)
+//                         operators (+ - * /)
+//                         ex. 11 - x + 5;
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//students++;
+//students--;
+
+console.log(students);
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
